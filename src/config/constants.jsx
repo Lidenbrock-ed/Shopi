bd51e5d8d54ac29056251d65e9678a796b2e4e35
Lidenbrock-ed <stivenh_1224@hotmail.com>
2025-03-05 21:03:13 -0500

    refactor: move menu options to config/constants.jsx

---
+++ src/config/constants.jsx
@@ -0,0 +1,45 @@
+export const leftMenu = [
+	{
+		path: '/',
+		name: 'All'
+	},
+	{
+		path: '/clothes',
+		name: 'Clothes'
+	},
+	{
+		path: '/electronics',
+		name: 'Electronics'
+	},
+	{
+		path: '/furnitures',
+		name: 'Furnitures'
+	},
+	{
+		path: '/toys',
+		name: 'Toys'
+	},
+	{
+		path: '/others',
+		name: 'Others'
+	}
+]
+
+export const rightMenu = [
+	{
+		path: '/my-account',
+		name: 'My Account'
+	},
+	{
+		path: '/my-order',
+		name: 'My Order'
+	},
+	{
+		path: '/my-orders',
+		name: 'My Orders'
+	},
+	{
+		path: '/sign-in',
+		name: 'Sign In'
+	}
+]
