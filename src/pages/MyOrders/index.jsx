a136deb53f510a24ec0b77e10f0d0e0f217e6c3d
Lidenbrock-ed <stivenh_1224@hotmail.com>
2025-03-13 20:31:10 -0500

    feat: add OrdersCard component and integrate with MyOrders page

--- src/pages/MyOrders/index.jsx
+++ src/pages/MyOrders/index.jsx
@@ -1,9 +1,27 @@
+import { useContext } from "react"
+import { Link } from "react-router-dom";
 import Layout from "../../components/Layout"
+import OrdersCard from "../../components/OrdersCard"
+import { ShoppingCartContext } from "../../context/shoppingCartContext";
 
 function MyOrders () {
+	const context = useContext(ShoppingCartContext)
+
     return (
 			<Layout>
-				My Orders
+				<div className="flex justify-center items-center relative w-80 mb-4">
+					<h1 className="font-medium text-xl">My Orders</h1>
+				</div>
+				{
+					context.order.map( (order)  => (
+						<Link key={order.id} to={`/my-orders/${order.id}`}>
+							<OrdersCard
+								totalPrice={order.totalPrice}
+								totalProducts={order.totalProducts}
+							/>
+						</Link>
+					))
+				}
 			</Layout>
 		)
 }
