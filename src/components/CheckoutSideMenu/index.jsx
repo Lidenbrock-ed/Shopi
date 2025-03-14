7c9b4de1cc291b7434b72a83b5100a5f860988ce
Lidenbrock-ed <stivenh_1224@hotmail.com>
2025-03-13 20:19:20 -0500

    feat: implement order state and handleCheckout function
    - Create Order state
    - Create handleCheckout function to redirect to /my-orders/last
    - Improve accesibility with proper ARIA attributes

--- src/components/CheckoutSideMenu/index.jsx
+++ src/components/CheckoutSideMenu/index.jsx
@@ -1,4 +1,5 @@
 import { useContext } from "react"
+import { Link } from "react-router-dom"
 import { IoCloseCircle } from "react-icons/io5"
 import { ShoppingCartContext } from "../../context/shoppingCartContext"
 import OrderCard from '../OrderCard/'
@@ -8,13 +9,24 @@ const CheckoutSideMenu = () => {
   const context = useContext(ShoppingCartContext)
 
   const handleDelete = (id) => {
-    const filteredProducts = context.cartProducts.filter( product => {
-      return product.id !== id
-    })
+    const filteredProducts = context.cartProducts.filter( product => product.id !== id)
     context.setCartProducts(filteredProducts)
-    context.setCount(context.count - 1)
+    context.setCount( prevCount => prevCount - 1)
   }
 
+  const handleCheckout = () => {
+    const orderToAdd = {
+      id: crypto.randomUUID(),
+      date: new Date(),
+      products: context.cartProducts,
+      totalProducts: context.cartProducts.length,
+      totalPrice: totalPrice(context.cartProducts)
+    }
+
+    context.setOrder([...context.order, orderToAdd])
+    context.setCartProducts([])
+    context.closeCheckoutSideMenu()
+  }
   return (
     <aside
       className={`${
@@ -28,9 +40,10 @@ const CheckoutSideMenu = () => {
         <IoCloseCircle
           className="text-black w-6 h-6 m-1 cursor-pointer"
           onClick={() => context.closeCheckoutSideMenu()}
+          aria-label="Close cart"
         />
       </div>
-      <div className="px-6 overflow-auto">
+      <div className="px-6 overflow-auto flex-1">
         {
           context.cartProducts.map( (product, index) => {
             return <OrderCard 
@@ -44,11 +57,20 @@ const CheckoutSideMenu = () => {
           })
         }
       </div>
-      <div className="px-6 absolute bottom-0 rounded-b-lg bg-white w-full h-15">
-        <p className="flex justify-between items-center h-full text-xl font-medium">
+      <div className="px-6 pb-6 pt-2 sticky bottom-0 rounded-b-lg bg-white w-full h-fit ">
+        <p className="flex justify-between items-center h-6 text-xl font-medium mb-2">
           <span>Total:</span>
           <span>${totalPrice(context.cartProducts)}</span>
         </p>
+        <Link to={"/my-orders/last"}>
+          <button
+            onClick={() => { handleCheckout()}}
+            className="w-full bg-black py-3 text-white font-medium rounded-lg"
+            aria-label="Proceed to checkout"
+          >
+            Checkout
+          </button>
+        </Link>
       </div>
     </aside>
   )
